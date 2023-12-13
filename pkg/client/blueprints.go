59542c560fb64b5fb45bf33fc531f06cc7d6d250
Roman Novatorov <rnovatorov@enapter.com>
2023-12-13 10:24:53 +0100

    add basic blueprints API

---
+++ pkg/client/blueprints.go
@@ -0,0 +1,39 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type BlueprintsAPI struct {
+	client *Client
+}
+
+func (b *BlueprintsAPI) Download(
+	ctx context.Context, blueprintID string,
+) ([]byte, error) {
+	const path = "/api/v1/download"
+	req, err := b.client.NewRequestWithContext(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, fmt.Errorf("create request: %w", err)
+	}
+
+	query := req.URL.Query()
+	query.Set("blueprint_id", blueprintID)
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := b.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("do request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	blueprintBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("read body: %w", err)
+	}
+
+	return blueprintBytes, nil
+}
